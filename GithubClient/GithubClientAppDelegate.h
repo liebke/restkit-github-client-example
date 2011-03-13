62b4203d81120485a1f3a958bc631572d5541085
David Edgar Liebke <david@thinkrelevance.com>
2011-03-13 11:59:47 -0400

    Initial Commit

---
+++ GithubClient/GithubClientAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GithubClientAppDelegate.h
+//  GithubClient
+//
+//  Created by David Edgar Liebke on 3/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GithubClientAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ GithubClient/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  GithubClient
+//
+//  Created by David Edgar Liebke on 3/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
